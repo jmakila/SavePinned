a47d65b10692102d905229df485a635f33466408
jmakila <jussi.makila@gmail.com>
2017-06-21 19:53:13 +0300

    Everything works

--- popup.js
+++ popup.js
@@ -1,17 +1,5 @@
 
-
-function guid() {
-	function s4() {
-		return Math.floor((1 + Math.random()) * 0x10000)
-			.toString(16)
-			.substring(1);
-	}
-	return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-		s4();
-}
-
 function save_set(name) {
-
 	var urilist = [];
 	chrome.tabs.query({
 		pinned: true,
@@ -22,16 +10,13 @@ function save_set(name) {
 		}
 		if (urilist.length > 0) {
 			var saveObj = {};
-			var uid = guid();
+			var uid = window.btoa(name);
 			saveObj[uid] = {
 				set_name: name,
 				tabs: urilist
 			};
 			chrome.storage.sync.set(saveObj, function () {
-
-				// reload page
 				window.location.href = "popup.html";
-
 			});
 		} else {
 			console.log('No pinned tabs found!');
@@ -39,35 +24,80 @@ function save_set(name) {
 	});
 }
 
+function load_set(id) {
+	chrome.storage.sync.get(id, function (set) {
+		var tabs = set[id].tabs;
+		chrome.tabs.query({
+			pinned: true,
+			currentWindow: true
+		}, function (cutabs) {
+			for (ind in cutabs) {
+				chrome.tabs.remove(cutabs[ind].id);
+			}
+			for (inx in tabs) {
+				chrome.tabs.create({
+					url: tabs[inx],
+					active: false,
+					pinned: true
+				});
+			}
+		});
+	});
+}
+
+function delete_set(id) {
+	chrome.storage.sync.remove(id, function () {
+		window.location.href = "popup.html";
+	});
+}
+
 function get_sets() {
 	chrome.storage.sync.get(null, function (sets) {
-
 		for (var property in sets) {
 			if (sets.hasOwnProperty(property)) {
-
-
-				console.log(property);
-				console.log(sets[property]);
 				var row = sets[property];
-
 				var template = '\
 				<div class="load-row" data-id="'+property+'">\
 					<span>'+row.set_name+'</span><button class="set-load">Load</button><img class="set-delete" src="images/delete_16.png">\
 				</div>';
-
 				var area = document.getElementById('load-area');
 				area.insertAdjacentHTML('beforeend', template);
-
+				var plcelement = document.getElementById('placeholder')
+				if (plcelement) plcelement.remove();
 			}
 		}
+
+		// load buttons
+		var loadbtns = document.getElementsByClassName('set-load');
+		for (var i=0; i < loadbtns.length; i++) {
+			loadbtns[i].addEventListener('click', function () {
+				var id = this.parentNode.dataset.id;
+				load_set(id);
+			});
+		}
+
+		// delete buttons
+		var deletebtns = document.getElementsByClassName('set-delete');
+		for (var j=0; j < deletebtns.length; j++) {
+			deletebtns[j].addEventListener('click', function () {
+				var id = this.parentNode.dataset.id;
+				delete_set(id);
+			});
+		}
+
 	});
 }
 
 
 document.getElementById('save-button').addEventListener('click', function () {
-
 	var name = document.getElementById('save-name').value;
 	if (name) save_set(name);
-
 });
+document.getElementById('save-name').addEventListener('keydown', function (event) {
+	if (event.keyCode == 13) {
+		var name = document.getElementById('save-name').value;
+		if (name) save_set(name);
+	}
+});
+document.getElementById('save-name').focus();
 document.addEventListener('DOMContentLoaded', get_sets);
