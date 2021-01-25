ff219ee32cf4e33d73de309a9270a87d279176ad
Matt Gabrenya <matt@buildyourweb.app>
2021-01-25 13:00:49 -0600

    fix: refactor autoloading to run on runtime.onStartup event, instead of window.onCreated event for improved cross-browser reliability.

--- background.js
+++ background.js
@@ -1,19 +1,8 @@
 // autoloading tab set
 browser.storage.local.clear();
 
-var listener = function (win) {
-    console.log('Listener activated');
-    browser.windows.getAll(null).then(function (wins) {
-        if (wins.length < 2 && win.type === 'normal') {
-            Sets.autoLoad(win.id);
-        }
+browser.runtime.onStartup.addListener(function () {
+    browser.windows.getCurrent().then(function (win) {
+        Sets.autoLoad(win.id);
     });
-}
-
-// will this always fire at start
-browser.windows.getCurrent().then(function (win) {
-    Sets.autoLoad(win.id);
-    
-    browser.windows.onCreated.removeListener(listener);
-    browser.windows.onCreated.addListener(listener);
 });
