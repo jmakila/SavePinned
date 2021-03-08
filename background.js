cc9737a72482330dcb80020b16e481fbe828ce17
Matt Gabrenya <matt@buildyourweb.app>
2021-03-08 14:57:49 -0700

    fix: revert changes to autoload that broke functionality in Mac OS. Add workaround for firefox support

--- background.js
+++ background.js
@@ -1,8 +1,32 @@
 // autoloading tab set
 browser.storage.local.clear();
 
-browser.runtime.onStartup.addListener(function () {
-    browser.windows.getCurrent().then(function (win) {
-        Sets.autoLoad(win.id);
-    });
-});
+let autoloadedOnInitialWindow = false;
+
+var listener = function (win) {
+  console.log("listener called");
+  autoloadedOnInitialWindow = true;
+
+  browser.windows.getAll(null).then(function (wins) {
+    console.log("all windows", wins);
+    if (wins.length < 2 && win.type === "normal") {
+      Sets.autoLoad(win.id);
+    }
+  });
+};
+
+if (!browser.windows.onCreated.hasListener(listener)) {
+  console.log("browser does not have listener, adding now");
+  browser.windows.onCreated.addListener(listener);
+}
+
+// Firefox workaround:
+//  browser.windows.onCreated fires before the extension is loaded in Firefox
+//  so we run autoload manually, only if it has not been run before
+if (!autoloadedOnInitialWindow) {
+  console.log("never autoloaded on initial window");
+  browser.windows.getCurrent().then(function (win) {
+    console.log("win is ", win);
+    listener(win);
+  });
+}
