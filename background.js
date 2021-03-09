6b4a04765b54c66dea076fef04bfb1337ffd8df8
Matt Gabrenya <matt@buildyourweb.app>
2021-03-09 10:47:27 -0700

    refactor: extract Autoload methods into their own scope in functions.js, call autoload manually if it hasn't been called before, on extension load

--- background.js
+++ background.js
@@ -1,32 +1,11 @@
 // autoloading tab set
 browser.storage.local.clear();
 
-let autoloadedOnInitialWindow = false;
-
-var listener = function (win) {
-  console.log("listener called");
-  autoloadedOnInitialWindow = true;
-
-  browser.windows.getAll(null).then(function (wins) {
-    console.log("all windows", wins);
-    if (wins.length < 2 && win.type === "normal") {
-      Sets.autoLoad(win.id);
-    }
-  });
-};
-
-if (!browser.windows.onCreated.hasListener(listener)) {
-  console.log("browser does not have listener, adding now");
-  browser.windows.onCreated.addListener(listener);
+if (!browser.windows.onCreated.hasListener(Autoload.windowCreated)) {
+  browser.windows.onCreated.addListener(Autoload.windowCreated);
 }
 
-// Firefox workaround:
-//  browser.windows.onCreated fires before the extension is loaded in Firefox
-//  so we run autoload manually, only if it has not been run before
-if (!autoloadedOnInitialWindow) {
-  console.log("never autoloaded on initial window");
-  browser.windows.getCurrent().then(function (win) {
-    console.log("win is ", win);
-    listener(win);
-  });
-}
+// Workaround:
+//  browser.windows.onCreated does not consistently fire in all browsers
+//  on the first window launched
+Autoload.manual();
